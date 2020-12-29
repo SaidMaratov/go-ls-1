8bab47faa3d051a5f5de6800c8551d4bc2cf3d1e
SaidMaratov <you@example.com>
2020-12-29 15:37:18 +0600

    feat: server.go

--- server/server.go
+++ server/server.go
@@ -11,8 +11,6 @@ import (
 
 const welcome = "Welcome to TCP-Chat!"
 
-var i = 0
-
 var Users map[int]authorization.User
 
 func CreatePort(num string) {
@@ -37,26 +35,29 @@ func CreatePort(num string) {
 }
 
 func handle(conn net.Conn) {
-
+	var i int
 	scanner := bufio.NewScanner(conn)
 	fmt.Fprintf(conn, welcome+"\n"+"[ENTER YOUR NAME]:")
 	// посмотреть позже
 	for scanner.Scan() {
+
 		name := scanner.Text()
 		user := authorization.CreateNewAccount(name)
 		if Users == nil {
 			Users = make(map[int]authorization.User)
+			i = 0
+		} else {
+			i = len(Users)
 		}
 		Users[i] = user
 		fmt.Fprintf(conn, "Hello, %s\n", Users[i])
 		fmt.Fprintf(conn, "%s has joined our chat...\n", Users[i])
 		fmt.Println(Users)
-		i++
 		break
 	}
 	for scanner.Scan() {
 		ln := scanner.Text()
-		fmt.Println(ln)
+		fmt.Printf("%v - %s\n", Users[i], ln)
 		fmt.Fprintf(conn, "%s\n", Users[i])
 	}
 	defer conn.Close()
